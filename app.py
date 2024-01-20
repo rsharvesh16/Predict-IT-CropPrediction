a1a249f3b811c3ddce05f514599d0af1470c437f
rsharvesh16 <rsharvesh16@gmail.com>
2024-01-21 00:00:46 +0530

    Chatbot backend ready

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 import pickle
 import numpy as np
 from flask import jsonify
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+from PIL import Image
+
+load_dotenv()
 
 app = Flask(__name__, static_folder="templates/assets")
 
@@ -15,8 +21,8 @@ def home():
     return render_template('index.html')
 
 # Define the prediction route
-@app.route('/Predict', methods=['POST', 'GET'])
-def Predict():
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
     if request.method == 'POST':
         # Get input values from the form
         features = [float(request.form['Nitrogen_value']),
@@ -34,16 +40,30 @@ def Predict():
        
         # Display the predicted crop on the result page
         return render_template('predict.html', prediction = prediction[0])
-    # return render_template('predict.html')  # This line ensures predict.html is rendered when method is GET
+    return render_template('predict.html')  # This line ensures predict.html is rendered when method is GET
 
 # Redirect to the predict.html page when the predict button is clicked
-@app.route('/predict_button_clicked')
-def predict_button_clicked():
-    return render_template('predict.html')
 
-@app.route('/predict.html')  # Add this route for predict.html
-def predict():
-    return render_template('predict.html')
+
+@app.route('/chatbot', methods = ['GET', 'POST'])
+def ai_chatbot():
+    os.getenv("GOOGLE_API_KEY")
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+    if request.method=='POST':
+        input_text = request.form['input_text']
+        print(input_text)
+        uploaded_file = request.files['uploaded_file']
+
+        model = genai.GenerativeModel('gemini-pro-vision')
+        image_parts = [{"mime_type": uploaded_file.mimetype, "data": uploaded_file.read()}]
+        print(image_parts)
+        response = model.generate_content([input_text, image_parts[0], ""])
+        print(response)
+
+        return render_template('chatbot.html', input_text = input_text, response=response.text)
+    return render_template('chatbot.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
